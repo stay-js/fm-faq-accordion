f954d8f34c8ff61483f8d42650f52b16d23eb3d1
Nagy Zétény <nagyzeteny6@gmail.com>
2023-12-01 13:11:35 +0100

    propper meta tags

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,18 @@
 import type { Metadata } from 'next';
 import { Accordion } from './accordion';
 
-const title = 'Frontend Mentor | Multi-step form';
+const title = 'Frontend Mentor - FAQ accordion solution';
 const description =
-  'This is a solution to the Multi-step form challenge on Frontend Mentor. Built with Next.js and Tailwind CSS.';
+  'This is a solution to the FAQ accordion challenge on Frontend Mentor. Built with Next.js and Tailwind CSS.';
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://fm-multi-step-form.znagy.hu'),
+  metadataBase: new URL('https://faq-accordion.znagy.hu'),
 
   authors: [{ name: 'Zétény Nagy', url: 'https://znagy.hu' }],
   creator: 'Zétény Nagy',
 
   keywords:
-    'frontend mentor, frontend, mentor, fem, age calculator, age, calculator, challenge, next.js, nextjs, react, react-hook-form, zod, headlessui, tailwind, tailwindcss',
+    'frontend mentor, frontend, mentor, fem, faq accordion, faq, accordion, challenge, next.js, nextjs, react, headlessui, tailwind, tailwindcss',
 
   title,
 
@@ -34,7 +34,7 @@ export const metadata: Metadata = {
 
   openGraph: {
     type: 'website',
-    url: 'https://fm-multi-step-form.znagy.hu',
+    url: 'https://faq-accordion.znagy.hu',
     title: description,
     siteName: title,
     locale: 'en-US',
